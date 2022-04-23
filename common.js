33b26e665f2507abb6af22e3d6658df04b99a1c6
Filip Šťastný <filip.stastny@avast.com>
2022-04-23 21:50:34 +0200

    get random int from interval

--- common.js
+++ common.js
@@ -130,3 +130,14 @@ function clearAllTimeouts() {
         clearTimeout(i);
     }
 }
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive). Source: https://stackoverflow.com/a/1527820/7684041
+ * The value is no lower than min (or the next integer greater than min if min isn't an integer)
+ * and no greater than max (or the next integer lower than max if max isn't an integer)
+ */
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
